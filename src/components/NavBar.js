e0d39a70aa846dc2cf84c55b5d905505b3514050
the-jey <jey.pley@gmail.com>
2020-03-07 21:49:23 +0100

    finish the navbar create the context and finish the product list

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
+import styled from 'styled-components';
+import { ButtonContainer } from './style/Button';
 
 export default class NavBar extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello from NavBar</h3>
-      </div>
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+        <Link to='/'>
+          <img src={logo} alt="Logo" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav aligns-item-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              Produits
+            </Link>
+          </li>
+        </ul>
+        <Link to='/cart' className="ml-auto">
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
+            Mon panier
+          </ButtonContainer>
+        </Link>
+      </NavWrapper>
     )
   }
 }
+
+// Styled Components
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link{
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 import Product from './Product';
+import Title from './shared/Title';
+import { ProductConsumer } from '../Context';
 
 export default class ProductList extends Component {
+
   render() {
     return (
-      <div>
-        <Product />
-      </div>
+
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="Nos" title="produits" />
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map( product => {
+                    return <Product key="{product.id}" product={product} />
+                  })
+                }}
+              </ProductConsumer>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+        // <Product />
     )
   }
 }
