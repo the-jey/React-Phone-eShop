2daec5896b7b9708b6e4d245b253c38cbd298450
the-jey <jey.pley@gmail.com>
2020-03-08 15:03:07 +0100

    add the paypal button

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -20,7 +20,7 @@ export default class Cart extends Component {
                     <Title name="Votre" title="Panier"></Title>
                     <CartColumns />
                     <CartList value={value} />
-                    <CartTotals value={value} />
+                    <CartTotals value={value} history={this.props.history} />
                   </div>
                 </React.Fragment>
               );
--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PaypalButton from './PaypalButton';
 
-export default function CartTotals({value}) {
+export default function CartTotals({value, history}) {
   const {cartSubTotal, cartTax, cartTotal, clearCart} = value;
 
   return (
     <React.Fragment>
       <div className="container">
         <div className="row">
-          <div className="col-md-12 col-sm-8 my-2  mx-auto  text-capitalize text-right">
+          <div className="col-md-12 col-sm-8 my-2  mx-auto  text-capitalize text-lg-right text--center">
             <Link to ="/">
               <button className="btn btn-outline-danger text-uppercase mb-3 px-5"
               type="button"
@@ -29,6 +30,7 @@ export default function CartTotals({value}) {
               <span className="text-title">Total : </span>
               <strong>{cartTotal} €</strong>
             </h5>
+            <PaypalButton total={cartTotal} clearCart={clearCart} history={history} />
           </div>
         </div>
       </div>
